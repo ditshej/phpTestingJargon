7c6a708c3ddee6adf765fd17c076f83a86d1b03a
Raphi W <ditshej@gmail.com>
2021-06-26 11:44:44 +0200

    Let PHPUnit Create Your Test Doubles

--- tests/SubscriptionTest.php
+++ tests/SubscriptionTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests;
 
+use App\Gateway;
 use App\Subscription;
 use App\User;
 use PHPUnit\Framework\TestCase;
@@ -18,8 +19,7 @@ class SubscriptionTest extends TestCase
     /** @test */
     public function creating_a_subscription_marks_the_user_as_subscribed() : void
     {
-        $gateway = new FakeGateway(); // don't user the actual Gateway. Use a dummy/fake version
-        $subscription = new Subscription($gateway);
+        $subscription = new Subscription($this->createMock(Gateway::class));
         $user = new User('John Doe');
 
         self::assertFalse($user->isSubscribed());
